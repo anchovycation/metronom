848d040ba40d25ae071355e5fcc26a749a232e6f
saracalihan <saracalihan@gmail.com>
2023-01-21 00:24:27 +0300

    IRedisAdaptor implemented with NodeRedis

---
+++ lib/IRedisAdaptor.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+interface IRedisAdaptor{
+  redisClient: any;
+  hGetAll(redisKey: string): Promise<object>;
+  hSet(redisKey: string, values:[string, any]): Promise<number>;
+  connect(): void;
+  keys(regex: string): Promise<Array<string>>;
+  del(redisKey: string): Promise<number>;
+  sendCommand(commands: Array<string>): Promise<unknown>;
+  echo(message: any):any
+}
+
+export default IRedisAdaptor;
